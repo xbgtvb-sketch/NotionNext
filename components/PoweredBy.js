42e5670b936327feafe77be62b1d8dbd8502ad82
xbgtvb-sketch <xbgtvb@gmail.com>
2026-06-29 17:14:35 +0800

    Update PoweredBy.js
    pw

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -9,11 +9,11 @@ export default function PoweredBy(props) {
     <div className={`inline text-sm font-serif ${props.className || ''}`}>
       <span className='mr-1'>Powered by</span>
       <a
-        href='https://github.com/tangly1024/NotionNext'
+        href='https://xinbg.pro'
         className='underline justify-start'>
-        NotionNext {siteConfig('VERSION')}
+        xinbg.pro
       </a>
-      .
+      
     </div>
   )
 }
