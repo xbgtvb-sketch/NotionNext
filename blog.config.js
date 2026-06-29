2e2e28b44dcef7f0d985d731f2e94ee45579d961
xbgtvb-sketch <xbgtvb@gmail.com>
2026-06-29 17:23:31 +0800

    Update blog.config.js
    cc

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'xinbg', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专注科技生活', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://xinbg.pro', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '科技生活, 博客', // 网站关键词 英文逗号隔开
